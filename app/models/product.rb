458aa5fc5f374da3beeaae7b4a25a35fccf16e81
Mihail <sergeenkov.mihail@gmail.com>
2015-09-30 20:50:55 +0300

    First page, books list

---
+++ app/models/product.rb
@@ -0,0 +1,15 @@
+class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+                with: %r{\.(gif|jpg|png)\Z}i,
+                message: 'URL must be for GIF, JPG, PNG image.'
+            #URL должен указывать на изображение формата GIF, JPG, PNG.
+                      }
+  validates_length_of :title, minimum: 10, message: 'Title must be greater than 10'
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+end
